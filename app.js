295ad2c520259afa4bbaf9a8ff8ddb1a3a7912ce
Have Samuel <samiehave9@gmail.com>
2023-02-28 02:19:04 +0300

    Operating the button

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// const form = document.querySelector('form');
+const input = document.querySelector('input');
 const btn = document.querySelector('button');
+
+const atpos = input.value.indexOf('@');
+const dotpos = input.value.lastIndexOf('.');
 
 btn.addEventListener('click', (e) => {
   e.preventDefault();
-  if (e.target.classList.toggle('btn')) {
-    console.log('Button clicked');
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= input.value.length) {
+    console.log('not valid');
+  } else {
+    console.log('valid');
+    throw new Error('not valid');
   }
 });
