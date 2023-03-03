f888a42f91b150b9b7788016f2cbfeb1901b2e7e
Have Samuel <samiehave9@gmail.com>
2023-03-03 22:30:04 +0300

    Clickable button

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-// const form = document.querySelector('form');
+const form = document.querySelector('form');
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
 
-// const atSign = input.value.indexOf('@');
-// const dotSign = input.value.lastIndexOf('.');
-
-btn.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const lowText = document.createElement('p');
-  lowText.innerText = input.value.toLowerCase();
-  lowText.classList.toggle('lowText');
-  input.innerText = 'Please provide a valid email';
-  input.classList.toggle('error');
-  input.appendChild(lowText);
-  input.value = '';
-  if (input.value === '') {
-    input.classList.toggle('error');
-  } else {
-    input.classList.remove('error');
-  }
+});
+
+btn.addEventListener('click', () => {
+  
 });
