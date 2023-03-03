f84bf6bf55f73568ffd5c54e6ecf620fb07e946b
Have Samuel <samiehave9@gmail.com>
2023-03-03 23:44:05 +0300

    Clickable button

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ const btn = document.querySelector('button');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+  btn.addEventListener('click', () => {
+    console.log('HELLO!');
+    if (input.value.trim() === '') {
+      input.innerText = 'Please provide a valid email ';
+      input.style.display = 'block';
+      return false;
+    }
+  });
 });
 
-btn.addEventListener('click', () => {
-  
-});
+// document.form['main-form'].onsubmit = (e) => {
+//   if (this.input.value.trim() === '') {
+//     document.querySelector('#email').innerHTML = 'Please provide a valid email';
+//     document.querySelector('#email').style.display = 'block';
+//     e.preventDefault();
+//     return false;
+//   }
+// };
