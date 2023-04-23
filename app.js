219a445757d70c695dabcbfdd56dfd3b9ec90180
Have Samuel <samiehave9@gmail.com>
2023-04-24 01:47:06 +0300

    Remove the unnecessary comments

--- app.js
+++ app.js
@@ -6,28 +6,22 @@ const roundIcon = document.querySelector('#red-note');
 // Email Validation with regular expression
 const isEmail = (email) => /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/.test(email);
 
-// function thatr wiil create an error
+// function that wiil create an error
 const highlightError = (emailInput, errorMessage) => {
   emailInput.classList.add('form__field--error');
-  // roundIcon.classList.add('round-icon');
   roundIcon.style.display = 'block';
-  // console.log(roundIcon);
   error.innerText = errorMessage;
 };
 
-// Function that will remove the error
 const clearError = (emailInput) => {
-  // roundIcon.classList.remove('round-icon');
   roundIcon.style.display = 'none';
   emailInput.classList.remove('form__field--error');
   error.innerText = '';
-  // console.log(emailInput);
 };
 
 const validate = () => {
   const emailValue = emailInput.value.trim();
 
-  // console.log(emailValue);
   clearError(emailInput);
 
   if (!emailValue) {
